dd69d36144c7b3a8df0a052ac3c6cb8fd28085e7
Glummy <58010838+Glumboi@users.noreply.github.com>
2025-02-13 10:00:51 +0100

    Added 'rm' command

---
+++ rm/src/main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+    if (argc <= 1)
+    {
+        fprintf(stderr, "No file/s for removal specified!\n");
+    }
+
+    for (size_t i = 1; i < argc; i++)
+    {
+        if (remove(argv[i]) == 0)
+        {
+            fprintf(stdout, "Deleted file: %s\n", argv[i]);
+            continue;
+        }
+
+        fprintf(stderr, "Failed to delete file: %s\n", argv[i]);
+    }
+
+    return 0;
+}
